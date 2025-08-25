4b8626d142b9781fcfc256d8eaaf5a590e004869
abtp2 <netraballabhpandey@gmail.com>
2025-08-25 07:21:39 +0530

    update

--- src/components/admin/AdminAnalytics.tsx
+++ src/components/admin/AdminAnalytics.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useRef, ChangeEvent, useEffect } from 'react';
-import { LuEye, LuTrendingUp, LuDatabase, LuBox, LuImage, } from 'react-icons/lu';
+import { LuEye, LuTrendingUp, LuDatabase, LuBox, LuImage, LuMousePointerClick, } from 'react-icons/lu';
 import { createClient } from '@/lib/supabase/client';
 import Styles from '@/app/admin/admin.module.css';
 import { type User } from '@supabase/supabase-js';
@@ -32,20 +32,41 @@ const AdminAnalytics: React.FC<Props> = ({admin}) => {
       </div>
 
       <br/><br/><br/>
+      
+      <h1><LuMousePointerClick/> <p>Clicks</p> <span>Clicks Overview</span></h1>
+      <div className={Styles.adminAnalyticsContainer}>
+        <div className={Styles.adminAnalyticsBox}>
+          <LuEye/>
+          <p>This Week</p>
+          <h2>1234</h2>
+        </div>
+        <div className={Styles.adminAnalyticsBox}>
+          <LuEye/>
+          <p>This Month</p>
+          <h2>5678</h2>
+        </div>
+        <div className={Styles.adminAnalyticsBox}>
+          <LuTrendingUp/>
+          <p>Total Views</p>
+          <h2>9739</h2>
+        </div>
+      </div>
+
+      <br/><br/><br/>
 
       <h1><LuDatabase/> <p>Storage</p> <span>Storage Overview</span></h1>
       <div className={Styles.adminAnalyticsContainer}>
         <div className={Styles.adminAnalyticsBox}>
           <LuBox/>
           <p>Supabase</p><br/>
-          <div>28% <span>Table</span></div><br/>
-          <div>16.29% <span>Image</span></div>
+          <div className={Styles.adminAnalyticsBar}>28% <span>Table</span></div><br/>
+          <div className={Styles.adminAnalyticsBar}>16.29% <span>Image</span></div>
         </div>
         <div className={Styles.adminAnalyticsBox}>
           <LuImage/>
           <p>ImageKit</p><br/>
-          <div>28% <span>Videos</span></div><br/>
-          <div>16.29% <span>Images</span></div>
+          <div className={Styles.adminAnalyticsBar}>28% <span>Videos</span></div><br/>
+          <div className={Styles.adminAnalyticsBar}>16.29% <span>Images</span></div>
         </div>
       </div>
     </div>
